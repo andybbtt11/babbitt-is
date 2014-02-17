f8af5895eaab9a6ba26e263be2e6c40f8603512e
Andy Babbitt <ababbitt@peapod.com>
2014-02-17 13:18:44 -0600

    db readonly

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-var dbUrl = "mongodb://andyblog:123123123@ds027748.mongolab.com:27748/blog";
+var dbUrl = "mongodb://genericuser:genericpw@ds027748.mongolab.com:27748/blog";
 //var dbUrl = "blog";
 var collections = ["posts"];
 
